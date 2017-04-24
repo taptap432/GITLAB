94f223e1d222827c8e5e5d150cadc4bb56d0ca7d
taptap432 <thitiya2007@hotmail.com>
2017-04-24 11:28:22 +0700

    Add new 3 file

---
+++ Lottery.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+<<<<<<< HEAD
+  int WinLotteryNo[10] = {1112, 0044, 0044, 0009, 5614, 8876, 1215, 4232, 9155, 9995}; 
+  float WinAmount[10] = {100000, 10000, 20000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
+
+  int N = 10;
+  int i, won; 
+  
+  int YourLotteryNo;  
+   
+  printf("Enter your Lottery Number (0 to end): "); 
+  scanf("%d", &YourLotteryNo); 
+  while (YourLotteryNo != 0) 
+  { 
+    won = 0; 
+    for (i=0;i<N && !won; i++) 
+    {
+       if (YourLotteryNo == WinLotteryNo[i]) 
+       {
+          printf("Congratulations You Won %8.2f baht\n", WinAmount[i]); 
+          won = 1;
+       }
+    }
+    if (!won) 
+      printf("Sorry Lottery Number %4d did not win\n", YourLotteryNo); 
+    printf("Enter your Lottery Number (0 to end): "); 
+    scanf("%d", &YourLotteryNo); 
+  }
+  printf("GOODBYE");
+  scanf("%d", &i);
+}
